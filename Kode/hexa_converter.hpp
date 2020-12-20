3aab177e07cce6d6243a35c2b7fede52bbf74e57
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2020-12-20 18:39:57 +0700

    Angka heksadesimal tidak case sensitive

--- Kode/hexa_converter.hpp
+++ Kode/hexa_converter.hpp
@@ -8,7 +8,7 @@ int h_ke_d(char heksadesimal[100])
     int hasil = 0;
     for (int i = 0; i < strlen(heksadesimal); i++)
     {
-        switch (heksadesimal[strlen(heksadesimal) - i])
+        switch (heksadesimal[strlen(heksadesimal) - i - 1])
         {
         case '0':
         {
@@ -61,31 +61,37 @@ int h_ke_d(char heksadesimal[100])
             break;
         }
         case 'A':
+        case 'a':
         {
             hasil += 10 * pow(16, i);
             break;
         }
         case 'B':
+        case 'b':  
         {
             hasil += 11 * pow(16, i);
             break;
         }
         case 'C':
+        case 'c':
         {
             hasil += 12 * pow(16, i);
             break;
         }
         case 'D':
+        case 'd':
         {
             hasil += 13 * pow(16, i);
             break;
         }
         case 'E':
+        case 'e':
         {
             hasil += 14 * pow(16, i);
             break;
         }
         case 'F':
+        case 'f':
         {
             hasil += 15 * pow(16, i);
             break;
