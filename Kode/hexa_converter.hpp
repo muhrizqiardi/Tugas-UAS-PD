5443fcafb2a9aea09ea239883e5db0b8f9773df3
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2020-12-20 19:31:04 +0700

    Menambahkan fungsi untuk mengkonversi desimal ke heksadesimal

--- Kode/hexa_converter.hpp
+++ Kode/hexa_converter.hpp
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <string>
 #include <math.h>
 using namespace std;
 
@@ -103,4 +104,97 @@ int h_ke_d(char heksadesimal[100])
     return hasil;
 }
 
+int d_ke_h(double desimal) {
+    string hasil = "";
+    for (double i = 0; i != 0 ; i = (int)i / 16)
+    {
+        int x = i/16-((int)i/16);
+        switch (x)
+        {
+        case 0: 
+        {
+            hasil = '0' + hasil;
+            break;
+        }
+        case 1: 
+        {
+            hasil = '1' + hasil;
+            break;
+        }
+        case 2: 
+        {
+            hasil = '2' + hasil;
+            break;
+        }
+        case 3: 
+        {
+            hasil = '3' + hasil;
+            break;
+        }
+        case 4: 
+        {
+            hasil = '4' + hasil;
+            break;
+        }
+        case 5: 
+        {
+            hasil = '5' + hasil;
+            break;
+        }
+        case 6: 
+        {
+            hasil = '6' + hasil;
+            break;
+        }
+        case 7: 
+        {
+            hasil = '7' + hasil;
+            break;
+        }
+        case 8: 
+        {
+            hasil = '8' + hasil;
+            break;
+        }
+        case 9: 
+        {
+            hasil = '9' + hasil;
+            break;
+        }
+        case 10: 
+        {
+            hasil = 'A' + hasil;
+            break;
+        }
+        case 11: 
+        {
+            hasil = 'B' + hasil;
+            break;
+        }
+        case 12: 
+        {
+            hasil = 'C' + hasil;
+            break;
+        }
+        case 13: 
+        {
+            hasil = 'D' + hasil;
+            break;
+        }
+        case 14: 
+        {
+            hasil = 'E' + hasil;
+            break;
+        }
+        case 15: 
+        {
+            hasil = 'F' + hasil;
+            break;
+        }
+        default:
+            break;
+        }
+    }
+    
+}
 // Konverter desimal ke heksadesimal
